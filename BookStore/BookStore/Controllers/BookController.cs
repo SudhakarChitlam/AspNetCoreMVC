e9d075aa80b9c5ab69a50d8c0ce7afc94ceb6078
Sudhakar <sksudha075@gmail.com>
2021-01-02 23:09:10 +0530

    Added CSS, Bootstrap

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.DataSource();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBooks(string bookname, string authorname)
+        {
+            return _bookRepository.SearchBook(bookname, authorname);
+        }
+    }
+}
---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string author, string title)
+        {
+            return DataSource().Where(x => x.Author.Contains(author) || x.Title.Contains(title)).ToList();
+        }
+        public List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1, Author="MVC" , Title="Sudh"},
+                new BookModel(){Id=2, Author="Dotnet" , Title="Ramesh"},
+                new BookModel(){Id=3, Author="Core" , Title="Suresh"},
+                new BookModel(){Id=4, Author="SQL" , Title="Naresh"}
+            };
+        }
+
+    }
+}
